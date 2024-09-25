99276ae608ab531d6a0d5f0e1c8cf0f4a2daa710
jae hyun <1994dbwogus@gmail.com>
2024-09-26 00:29:45 +0900

    feat: PointRepository 신규 정의

---
+++ src/point/point.repository.ts
@@ -0,0 +1,52 @@
+import { PointHistoryTable } from 'src/database/pointhistory.table';
+import { UserPointTable } from 'src/database/userpoint.table';
+import { PointHistory, UserPoint } from './point.model';
+
+type InsertPointBody = Pick<PointHistory, 'amount' | 'type'>;
+
+export abstract class PointRepositoryPort {
+  abstract findPointBy(userId: number): Promise<UserPoint>;
+
+  abstract findHistoriesBy(userId: number): Promise<PointHistory[]>;
+
+  /**
+   * 포인트 생성 트랜잭션
+   * - PointHistory를 생성한다.
+   * - UserPoint를 최신화 한다.
+   * @param userId
+   * @param body
+   */
+  abstract insertPointWithTransaction(
+    userId: number,
+    body: InsertPointBody,
+  ): Promise<UserPoint>;
+}
+
+export class PointRepository extends PointRepositoryPort {
+  constructor(
+    private readonly userDb: UserPointTable,
+    private readonly historyDb: PointHistoryTable,
+  ) {
+    super();
+  }
+
+  override async findPointBy(userId: number): Promise<UserPoint> {
+    return await this.userDb.selectById(userId);
+  }
+
+  override async findHistoriesBy(userId: number): Promise<PointHistory[]> {
+    return await this.historyDb.selectAllByUserId(userId);
+  }
+
+  override async insertPointWithTransaction(
+    userId: number,
+    body: InsertPointBody,
+  ): Promise<UserPoint> {
+    const { amount, type } = body;
+    await this.historyDb.insert(userId, amount, type, Date.now());
+    const userPoint = await this.userDb.selectById(userId);
+
+    const updatePoint = userPoint.point + amount;
+    return await this.userDb.insertOrUpdate(userId, updatePoint);
+  }
+}
